25344d3ce74a8dbb083e0dabd52d76b7eaec4b2e
eunjinlee <lucy7xoxo@naver.com>
2021-06-12 23:01:39 +0900

    대화내용 실시간으로 보여주도록 기능추가

--- src/messages/Messages.vue
+++ src/messages/Messages.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <SingleMessage />
+    <SingleMessage :messages="messages" />
     <MessageForm />
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
 import SingleMessage from "./SingleMessage";
 import MessageForm from "./MessageForm";
 import database from "firebase/database";
@@ -18,8 +19,38 @@ export default {
   },
   data() {
     return {
-      messagesRef: firebase.database().ref("messages")
+      messagesRef: firebase.database().ref("messages"),
+      messages: [],
+      channel: ""
     };
+  },
+  computed: {
+    ...mapGetters(["currentChannel"])
+  },
+  watch: {
+    currentChannel: function() {
+      // 현재 채널이 변경되면 그 채널의 메시지만을 가져옴.
+      this.messages = [];
+      this.addListeners();
+      this.channel = this.currentChannel;
+    }
+  },
+  methods: {
+    addListeners() {
+      this.messagesRef
+        .child(this.currentChannel.id)
+        .on("child_added", snapshot => {
+          this.messages.push(snapshot.val());
+        });
+    },
+    detatchListeners() {
+      if (this.channel !== null) {
+        this.messagesRef.child(this.channel.id).off();
+      }
+    }
+  },
+  beforeDestroy() {
+    this.detatchListeners();
   }
 };
 </script>
--- src/messages/SingleMessage.vue
+++ src/messages/SingleMessage.vue
@@ -1,11 +1,29 @@
 <template>
   <div>
-    single message component
+    <div class="mt-3">
+      <div v-for="(message, index) in messages" :key="index">
+        <div class="media">
+          <img
+            :src="message.user.avatar"
+            height="50"
+            :alt="message.user.id"
+            class="align-self-start mr-3"
+          />
+          <div class="media-body">
+            <h6 class="mt-0">
+              <a href="#">{{ message.user.name }}</a> - {{ message.timestamp }}
+            </h6>
+            <p>{{ message.content }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "SingleMessage"
+  name: "SingleMessage",
+  props: ["messages"]
 };
 </script>
